d3afba2954e6cf2bcf96e4cf51dc522553c3624c
Parth Choudhary <parth.choudhary@gmail.com>
2022-07-06 20:09:15 +0530

    fixing card results

--- pages/index.tsx
+++ pages/index.tsx
@@ -313,7 +313,7 @@ export default function Home() {
     //if(true){
         return (
             <div style={{display:'flex', width:'100%', height:'100%', justifyContent:'center', alignItems:'center', flexDirection:'column'}}>
-              <BallTriangle ariaLabel="loading-indicator" color="white" width="400" height="500" />
+              {/* <BallTriangle ariaLabel="loading-indicator" color="white" width="400" height="500" /> */}
               <div className="shsimmer" style={{color:'white', fontSize: 66}}>
                 Loading!
               </div>
